e1669ccaf5647ed00c4d09babc917aee8ed6d26f
jun.hinata <rok3war1kam2mob2043@outolook.jp>
2020-06-21 15:50:19 +0900

    Add css

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -49,7 +49,7 @@ public class IndexServlet extends HttpServlet {
                              .getResultList();
 
         // 全件数を取得
-        long tasks_count = (long)em.createNamedQuery("getTasksCount", long.class)
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
                                    .getSingleResult();
         em.close();
 
